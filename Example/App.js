1b889bb3276b8bcbf4c24665538575b5a8d97f5b
PatrissolJuns <patrissolkenfack@gmail.com>
2020-09-25 04:27:17 +0100

    Configured toast

--- Example/App.js
+++ Example/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 import Navigator from "./src/Navigator";
+import {RootSiblingParent} from 'react-native-root-siblings';
 
 const App = () => {
   return (
-    <>
+    <RootSiblingParent>
       <StatusBar barStyle="dark-content" />
       <Navigator />
-    </>
+    </RootSiblingParent>
   );
 };
 
---
+++ Example/src/toast.js
@@ -0,0 +1,29 @@
+import Toast from 'react-native-root-toast';
+
+const toast =  {
+    error(text, callback = null) {
+        let toast = Toast.show(text, {
+            duration: Toast.durations.LONG,
+            position: Toast.positions.BOTTOM,
+            shadow: true,
+            animation: true,
+            hideOnPress: true,
+            backgroundColor: 'red',
+            delay: 0,
+            onShow: () => {
+                // calls on toast\`s appear animation start
+            },
+            onShown: () => {
+                // calls on toast\`s appear animation end.
+            },
+            onHide: () => {
+                // calls on toast\`s hide animation start.
+            },
+            onHidden: () => {
+                if (callback) callback();
+            }
+        });
+    }
+};
+
+export default toast;
